676c90feddbd522d3fd10cb3fcf8203d862dbdec
ZHANGyuan <fern35121321@outlook.com>
2018-01-07 15:32:22 +0100

    correct the errors of sortPack

--- TF_IDF/src/pack/SortPack.java
+++ TF_IDF/src/pack/SortPack.java
@@ -1,6 +1,9 @@
 package pack;
 
 import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.DoubleWritable.Comparator;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -8,33 +11,30 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class SortPack {
 
-    public static class SortMapper extends Mapper<Object, Text, Text, Text> {
-
-        protected void map(Object key, Text value, Mapper<Object, Text, Text, Text>.Context context)
+    public static class SortMapper extends Mapper<Object, Text,DoubleWritable, Text> {
+    	private DoubleWritable tfidf_double=new DoubleWritable();
+    	private Text newvalue = new Text();
+    	
+        protected void map(Object key, Text value,Context context)
                 throws IOException, InterruptedException {
             String str[]=value.toString().split("\t");
-            context.write(new Text(str[1]), new Text(str[0]));
+            tfidf_double.set(Double.parseDouble(str[1]));
+            newvalue.set(str[0]);
+            context.write(tfidf_double, newvalue);
         }
     }
     
-    public static class SortReducer extends Reducer<Text, Text, Text, Text> {
-        
-        private Text label = new Text();
-
-        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context)
+    public static class SortReducer extends Reducer<DoubleWritable, Text, Text,DoubleWritable> {
+    	
+        protected void reduce(DoubleWritable key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
-            for(Text val : values){  
-                label.set(val);  
-                context.write(label, key);  
+            for(Text val : values){   
+                context.write(val, key);  
             }
         }
     }
     
-    public static class DescComparator extends WritableComparator{  
-    	  
-        protected DescComparator() {  
-            super(Text.class,true);  
-        }  
+    public static class DescComparator extends Comparator{  
 
         public int compare(byte[] arg0, int arg1, int arg2, byte[] arg3,  
                 int arg4, int arg5) {  
